c24a26ef9af280d62744b41cedb92f51bb92ae12
ozkan <wallade1912@gmail.com>
2017-06-28 01:21:26 +0300

    container div col-lg-10

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <app-header></app-header>
     <div class="container">
       <div class="row">
-        <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+        <div class="col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1">
           <router-view></router-view>
         </div>
       </div>
